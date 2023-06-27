57f7098df07c81ae1df38eecb1ac1028541b07fe
arunmalewadi@gmail.com <arunmalewadi@gmail.com>
2023-06-27 13:37:10 +0530

    increament decrement by 1

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,27 @@
 import classes from './Counter.module.css';
+import {useDispatch, useSelector} from 'react-redux'
+
 
 const Counter = () => {
+  const counter =useSelector((state)=>state.counter)
+  const Dispatch=useDispatch()
+  const IncreamentHandler=()=>{
+Dispatch({type:"Increament"})
+
+  }
+  const DecreamentHandler=()=>{
+  Dispatch({type:"Decreamenr"})
+  }
+  
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <button onClick={IncreamentHandler}>Increament</button>
+      <button onClick={DecreamentHandler}>Decreament</button>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
