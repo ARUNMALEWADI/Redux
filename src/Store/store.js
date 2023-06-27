f9f837da5354ced67518965b50378c7c0f126080
arunmalewadi@gmail.com <arunmalewadi@gmail.com>
2023-06-27 14:37:39 +0530

    increament by 2

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,11 +1,11 @@
 import {createStore} from 'react-redux'
 const reducer=(state={counter:0},action)=>{
 if(action.type==="Increament")
-{   return {counter:state.counter+1}
+{   return {counter:state.counter+2}
 
 }
 if(action.type==="Decreament")
-{ return {counter:state.counter-1}
+{ return {counter:state.counter-2}
 
 }
 return state
